8ca664038612eac5ca27f8228a54c427060b6fe9
Andre Rocha <99468080+andrerochadsr@users.noreply.github.com>
2022-10-09 21:40:19 -0300

    correções gramaticas

--- minmax.js
+++ minmax.js
@@ -3,25 +3,22 @@ function ligar() {
     let numMin_Minimo = parseInt(valorNumMin_Minimo.value)
     let valorNumMax_Minimo = window.document.querySelector("#inumMax-valorMinimo")
     let numMax_Minimo =  parseInt(valorNumMax_Minimo.value)
-
     let valorNumMin_Maximo = window.document.querySelector("#inumMin-valorMaximo")
     let numMin_Maximo = parseInt(valorNumMin_Maximo.value)
     let valorNumMax_Maximo = window.document.querySelector("#inumMax-valorMaximo")
     let numMax_Maximo = parseInt(valorNumMax_Maximo.value)
-    //window.alert(numMax_Maximo.value)
 
     if (numMax_Minimo > numMin_Minimo && numMin_Maximo >= numMax_Minimo && numMax_Maximo > numMin_Maximo)  {
-        function minMaxlist() { //função para retornar o valor maximo e minimo de uma lista gerada com o metodo random
-            let listaNum = []; // array que conterá os numeros aleatorios
-            randNumMax = parseInt(Math.random() * (numMax_Maximo - numMin_Maximo) + numMin_Maximo); // cria um numero aleatorio maximo
-            randNumMin = parseInt(Math.random() * (numMax_Minimo - numMin_Minimo) + numMin_Minimo); // cria um numero aleatorio minimo
-            for (let i = randNumMin; i <= randNumMax; i++) { // laço criado para adicionar os numero no Array "listaNum" baseado nos numeros gerados acima
+        function minMaxlist() { //função para retornar o valor máximo e mínimo de uma lista gerada com o método random.
+            let listaNum = []; // array que conterá os números aleatórios.
+            randNumMax = parseInt(Math.random() * (numMax_Maximo - numMin_Maximo) + numMin_Maximo); // cria um numero aleatório máximo.
+            randNumMin = parseInt(Math.random() * (numMax_Minimo - numMin_Minimo) + numMin_Minimo); // cria um numero aleatório mínimo.
+            for (let i = randNumMin; i <= randNumMax; i++) { // laço criado para adicionar os números no Array "listaNum" baseado nos números gerados acima.
                 listaNum.push(i)};
-            return `o valor minimo da lista é ${Math.min.apply(null, listaNum)} e o valor maximo é ${Math.max.apply(null, listaNum)}`; // retorna o valor minimo e maximo
+            return `o valor mínimo da lista é ${Math.min.apply(null, listaNum)} e o valor máximo é ${Math.max.apply(null, listaNum)}.`; // retorna o valor mínimo e máximo.
         }
         let texto = minMaxlist()
         resultado.innerText = texto
-        //console.log(minMaxlist())
         
     } else {
         resultado.innerText = "ERRO"
