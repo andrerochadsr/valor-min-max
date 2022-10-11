65774c42cbca6b6664fbab4940b67a79465181b8
Andre Rocha <99468080+andrerochadsr@users.noreply.github.com>
2022-10-11 20:24:23 -0300

    correções

--- minmax.js
+++ minmax.js
@@ -2,15 +2,15 @@ function gerar() {
     if (document.querySelector("#inumMin-valorMinimo").value == '' || document.querySelector("#inumMax-valorMinimo").value == '' || document.querySelector("#inumMin-valorMaximo").value == '' || document.querySelector("#inumMax-valorMaximo").value == '') /*verifica se os campos estao vazios*/ {
         resultado.innerText = "Preencha todos os campos."
     } else {
-        let valorNumMin_Minimo = window.document.querySelector("#inumMin-valorMinimo")
-        let numMin_Minimo = parseInt(valorNumMin_Minimo.value)
-        let valorNumMax_Minimo = window.document.querySelector("#inumMax-valorMinimo")
-        let numMax_Minimo =  parseInt(valorNumMax_Minimo.value)
-        let valorNumMin_Maximo = window.document.querySelector("#inumMin-valorMaximo")
-        let numMin_Maximo = parseInt(valorNumMin_Maximo.value)
-        let valorNumMax_Maximo = window.document.querySelector("#inumMax-valorMaximo")
-        let numMax_Maximo = parseInt(valorNumMax_Maximo.value)
-    
+        let valorNumMin_Minimo = window.document.querySelector("#inumMin-valorMinimo");
+        let numMin_Minimo = parseInt(valorNumMin_Minimo.value);
+        let valorNumMax_Minimo = window.document.querySelector("#inumMax-valorMinimo");
+        let numMax_Minimo =  parseInt(valorNumMax_Minimo.value);
+        let valorNumMin_Maximo = window.document.querySelector("#inumMin-valorMaximo");
+        let numMin_Maximo = parseInt(valorNumMin_Maximo.value);
+        let valorNumMax_Maximo = window.document.querySelector("#inumMax-valorMaximo");
+        let numMax_Maximo = parseInt(valorNumMax_Maximo.value);
+
         if (numMax_Minimo > numMin_Minimo && numMin_Maximo >= numMax_Minimo && numMax_Maximo > numMin_Maximo)  {
             function minMaxlist() { //função para retornar o valor máximo e mínimo de uma lista gerada com o método random.
                 let arrayNum = []; // array que conterá os números aleatórios.
@@ -19,15 +19,14 @@ function gerar() {
                 for (let i = randNumMin; i <= randNumMax; i++) { // laço criado para adicionar os números no Array "arrayNum" baseado nos números gerados acima.
                     arrayNum.push(i)};
                 return `o valor mínimo da lista é ${Math.min.apply(null, arrayNum)} e o valor máximo é ${Math.max.apply(null, arrayNum)}. A quantidade de números gerados foi de ${arrayNum.length}.`; // retorna o valor mínimo e máximo.
-            }
+            };
             
             resultado.innerText = minMaxlist() // escreve na div de class "container-resultado" o RETURN da função.
         } else {
             resultado.innerText = "ERRO! Preencha os campos de forma lógica."
-        }
-    }
-
-}
+        };
+    };
+};
 
 function gerarAuto() {
     function minMaxlist() { //função para retornar o valor máximo e mínimo de uma lista gerada com o método random.
@@ -37,7 +36,7 @@ function gerarAuto() {
         for (let i = randNumMin; i <= randNumMax; i++) { // laço criado para adicionar os números no Array "arrayNum" baseado nos números gerados acima.
             arrayNum.push(i)};
         return `o valor mínimo da lista é ${Math.min.apply(null, arrayNum)} e o valor máximo é ${Math.max.apply(null, arrayNum)}. A quantidade de números gerados foi de ${arrayNum.length}.`; // retorna o valor mínimo e máximo.
-    }
+    };
     
-    resultado.innerText = minMaxlist() // escreve na div de class "container-resultado" o RETURN da função.
-}
+    resultado.innerText = minMaxlist(); // escreve na div de class "container-resultado" o RETURN da função.
+};
